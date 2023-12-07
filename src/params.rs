2c85350346328ebc21585f22d8bdab8c95e319fb
Sen Jiang <jayxon@gmail.com>
2023-12-06 16:55:21 -0800

    DFS after MAX_CACHE_LENGTH

--- src/params.rs
+++ src/params.rs
@@ -30,6 +30,7 @@ pub fn match_goal(output: &Vector, _expr: &Expr) -> bool {
 pub const GOAL: &[Num] = &[1, -1, 0, 0];
 
 pub const MAX_LENGTH: usize = 14;
+pub const MAX_CACHE_LENGTH: usize = 10;
 pub const LITERALS: &[Num] = &[
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
     27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
