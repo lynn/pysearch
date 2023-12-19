d6d6b6dfcd98e1711dba457bce215a5f010597cd
Sen Jiang <jayxon@gmail.com>
2023-12-18 22:15:17 -0800

    Choose multithread based on level
    Multithreading is faster but uses more memory when building cache, so
    the ideal trade off is to use a single thread up to MAX_CACHE_LENGTH to
    save memory and then DFS using multithread.

--- src/params.rs
+++ src/params.rs
@@ -31,6 +31,7 @@ pub const GOAL: &[Num] = &[1, -1, 0, 0];
 
 pub const MAX_LENGTH: usize = 14;
 pub const MAX_CACHE_LENGTH: usize = 10;
+pub const MIN_MULTITHREAD_LENGTH: usize = MAX_CACHE_LENGTH + 1;
 pub const LITERALS: &[Num] = &[
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
     27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
